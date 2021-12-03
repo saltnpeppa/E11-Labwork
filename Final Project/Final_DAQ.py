483b8feb432f15d62e644b78d9750db9e49a7188
matthewfeiz <90935291+matthewfeiz@users.noreply.github.com>
2021-12-03 13:53:02 -0800

    Update Final_DAQ.py

--- Final Project/Final_DAQ.py
+++ Final Project/Final_DAQ.py
@@ -1,6 +1,13 @@
 import RPi.GPIO as GPIO
 import time as time
 import sys
+import numpy as np
+import math
+import statistics
+import matplotlib
+%matplotlib inline
+import matplotlib.pylot as plt
+
 
 class Final:
 
@@ -53,8 +60,25 @@ class Final:
         time.sleep(1)
 
 
-    def parse_data():
-        pass
+    def parse_data(self):
+        data_mean = np.mean(self.data["Counts"])
+        data_std = np.std(self.data["Counts"])
+        print(f"The relative uncertainty is: {data_std/data/mean}")
+        # equation for uncertainty effeciency std_f = f * sqrt((std_a/A)^2 + (std_b/B)^2) Where f = uncertainty, a = CPS, b = Determined Activity
+        #print(uncertainty_efficiency, "= uncertainty of the efficiency of the detector")
+        # Activity equation: A = 1 / (std_e/e)^2 + (std_CPS/CPS)^2
+        #print("The activity of the unknown source is:", A)
+        
+        #thorium_mean = 
+        #thorium_std = 
+        #thorium_activity = 
+        #other_source_mean = 
+        #other_source_std = 
+        #other_source_activity = 
+        
+        
+        
+        
         
     
     
